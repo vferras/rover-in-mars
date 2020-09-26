2b25f0663caa878320e5981511d9d6d2948b49f5
vferras <vferras@hotmail.com>
2020-09-26 20:09:08 +0200

    Shift to a more functional paradigm
    Top level functions without using any do-while

--- src/MarsRover.kt
+++ src/MarsRover.kt
@@ -6,69 +6,74 @@ fun main() {
     val initialHorizontalMapSize = printAndRead<Int>("Insert horizontal map size:", reader)
     val initialVerticalMapSize = printAndRead<Int>("Insert vertical map size:", reader)
 
-    var roverPositionX = printAndRead<Int>("Insert horizontal initial rover position:", reader)
-    var roverPositionY = printAndRead<Int>("Insert vertical initial rover position:", reader)
-    var roverPositionZ = printAndRead<String>("Insert initial rover direction:", reader)
+    var initialRoverPositionX = printAndRead<Int>("Insert horizontal initial rover position:", reader)
+    var initialRoverPositionY = printAndRead<Int>("Insert vertical initial rover position:", reader)
+    var initialRoverPositionZ = printAndRead<String>("Insert initial rover direction:", reader)
 
-    do {
-        println("Insert command (f = forward, b = backward, l = turn left, r = turn right):")
-
-        when(reader.next()) {
-            "f" -> {
-                val (newRoverPositionX, newRoverPositionY) = whenForward(roverPositionZ, roverPositionX, roverPositionY)
-                roverPositionX = newRoverPositionX
-                roverPositionY = newRoverPositionY
-            }
-            "b" -> {
-                val (newRoverPositionX, newRoverPositionY) = whenBackward(roverPositionZ, roverPositionX, roverPositionY)
-                roverPositionX = newRoverPositionX
-                roverPositionY = newRoverPositionY
-            }
-            "l" -> roverPositionZ = whenLeft(roverPositionZ)
-            "r" -> roverPositionZ = whenRight(roverPositionZ)
-        }
-
-        println(String.format("Rover is at x:%d y:%d facing:%s", roverPositionX, roverPositionY, roverPositionZ))
-    } while (true)
+    readInputAndMove(reader, initialRoverPositionZ, initialRoverPositionX, initialRoverPositionY)
 }
 
-private fun whenForward(currentPositionZ: String, currentPositionX: Int, currentPositionY: Int): Pair<Int, Int> {
+fun readInputAndMove(reader: Scanner, positionZ: String, positionX: Int, positionY: Int) {
+    println("Insert command (f = forward, b = backward, l = turn left, r = turn right):")
+
+    val result = when(reader.next()) {
+        "f" -> {
+            val (newPositionX, newPositionY) = whenForward(positionZ, positionX, positionY)
+            Triple(positionZ, newPositionX, newPositionY)
+        }
+        "b" -> {
+            val (newPositionX, newPositionY) = whenBackward(positionZ, positionX, positionY)
+            Triple(positionZ, newPositionX, newPositionY)
+        }
+        "l" -> Triple(whenLeft(positionZ), positionX, positionY)
+        "r" -> Triple(whenRight(positionZ), positionX, positionY)
+        else -> {
+            println("Incorrect direction. Try again.")
+            Triple(positionZ, positionX, positionY)
+        }
+    }
+
+    println(String.format("Rover is at x:%d y:%d facing:%s", result.second, result.third, result.first))
+    readInputAndMove(reader = reader, positionZ = result.first, positionX = result.second, positionY = result.third)
+}
+
+fun whenForward(currentPositionZ: String, currentPositionX: Int, currentPositionY: Int): Pair<Int, Int> {
     return when(currentPositionZ) {
         "n" -> Pair(currentPositionX, currentPositionY + 1)
         "s" -> Pair(currentPositionX, currentPositionY - 1)
         "e" -> Pair(currentPositionX + 1, currentPositionY)
         "w" -> Pair(currentPositionX - 1, currentPositionY)
-        else -> Pair(0, 0)
+        else -> Pair(currentPositionX, currentPositionY)
     }
 }
 
-private fun whenBackward(currentPositionZ: String, currentPositionX: Int, currentPositionY: Int): Pair<Int, Int> {
+fun whenBackward(currentPositionZ: String, currentPositionX: Int, currentPositionY: Int): Pair<Int, Int> {
     return when(currentPositionZ) {
         "n" -> Pair(currentPositionX, currentPositionY - 1)
         "s" -> Pair(currentPositionX, currentPositionY + 1)
         "e" -> Pair(currentPositionX - 1, currentPositionY)
         "w" -> Pair(currentPositionX + 1, currentPositionY)
-        else -> Pair(0, 0)
+        else -> Pair(currentPositionX, currentPositionY)
     }
 }
 
-private fun whenLeft(currentPositionZ: String): String {
+fun whenLeft(currentPositionZ: String): String {
     return when(currentPositionZ) {
         "n" -> "w"
         "s" -> "e"
         "e" -> "n"
         "w" -> "s"
-        else -> ""
+        else -> currentPositionZ
     }
 }
 
-private fun whenRight(currentPositionZ: String): String {
+fun whenRight(currentPositionZ: String): String {
     return when(currentPositionZ) {
         "n" -> "e"
         "s" -> "w"
         "e" -> "s"
         "w" -> "n"
-        else -> ""
+        else -> currentPositionZ
     }
 }
 
