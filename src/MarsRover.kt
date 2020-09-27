997bd0504211d8d6e27c8fd2153b162e14cd86cc
vferras <vferras@hotmail.com>
2020-09-27 11:26:13 +0200

    Refactor to improve the solution
    - Recursive optimization
    - Map data structure to store the points and avoid having too many parameters

--- src/MarsRover.kt
+++ src/MarsRover.kt
@@ -3,77 +3,82 @@ import java.util.*
 fun main() {
     val reader = Scanner(System.`in`)
 
-    val initialHorizontalMapSize = printAndRead<Int>("Insert horizontal map size:", reader)
-    val initialVerticalMapSize = printAndRead<Int>("Insert vertical map size:", reader)
+    val points = mapOf(
+            "mapSizeX" to printAndRead<Int>("Insert horizontal map size:", reader),
+            "mapSizeY" to printAndRead<Int>("Insert vertical map size:", reader),
+            "x" to printAndRead<Int>("Insert horizontal initial rover position:", reader),
+            "y" to printAndRead<Int>("Insert vertical initial rover position:", reader),
+            "z" to printAndRead<String>("Insert initial rover direction:", reader)
+    )
 
-    var initialRoverPositionX = printAndRead<Int>("Insert horizontal initial rover position:", reader)
-    var initialRoverPositionY = printAndRead<Int>("Insert vertical initial rover position:", reader)
-    var initialRoverPositionZ = printAndRead<String>("Insert initial rover direction:", reader)
-
-    readInputAndMove(reader, initialRoverPositionZ, initialRoverPositionX, initialRoverPositionY)
+    readInputAndMove(reader, points)
 }
 
-fun readInputAndMove(reader: Scanner, positionZ: String, positionX: Int, positionY: Int) {
-    println("Insert command (f = forward, b = backward, l = turn left, r = turn right):")
+fun readInputAndMove(reader: Scanner, points: Map<String, Any>) {
+    tailrec fun recursive(reader: Scanner, points: Map<String, Any>) {
+        println("Insert command (f = forward, b = backward, l = turn left, r = turn right):")
 
-    val result = when(reader.next()) {
-        "f" -> {
-            val (newPositionX, newPositionY) = whenForward(positionZ, positionX, positionY)
-            Triple(positionZ, newPositionX, newPositionY)
+        val result = when(reader.next()) {
+            "f" -> {
+                val (newPositionX, newPositionY) = whenForward(points)
+                mapOf("z" to points["z"].toString(), "x" to newPositionX, "y" to newPositionY)
+            }
+            "b" -> {
+                val (newPositionX, newPositionY) = whenBackward(points)
+                mapOf("z" to points["z"].toString(), "x" to newPositionX, "y" to newPositionY)
+            }
+            "l" -> mapOf("z" to whenLeft(points), "x" to points["x"] as Int, "y" to points["y"] as Int)
+            "r" -> mapOf("z" to whenRight(points), "x" to points["x"] as Int, "y" to points["y"] as Int)
+            else -> {
+                println("Incorrect direction. Try again.")
+                points
+            }
         }
-        "b" -> {
-            val (newPositionX, newPositionY) = whenBackward(positionZ, positionX, positionY)
-            Triple(positionZ, newPositionX, newPositionY)
-        }
-        "l" -> Triple(whenLeft(positionZ), positionX, positionY)
-        "r" -> Triple(whenRight(positionZ), positionX, positionY)
-        else -> {
-            println("Incorrect direction. Try again.")
-            Triple(positionZ, positionX, positionY)
-        }
+
+        println(String.format("Rover is at x:%d y:%d facing:%s", result["x"], result["y"], result["z"]))
+        recursive(reader = reader, result)
     }
 
-    println(String.format("Rover is at x:%d y:%d facing:%s", result.second, result.third, result.first))
-    readInputAndMove(reader = reader, positionZ = result.first, positionX = result.second, positionY = result.third)
+    return recursive(reader, points)
 }
 
-fun whenForward(currentPositionZ: String, currentPositionX: Int, currentPositionY: Int): Pair<Int, Int> {
-    return when(currentPositionZ) {
-        "n" -> Pair(currentPositionX, currentPositionY + 1)
-        "s" -> Pair(currentPositionX, currentPositionY - 1)
-        "e" -> Pair(currentPositionX + 1, currentPositionY)
-        "w" -> Pair(currentPositionX - 1, currentPositionY)
-        else -> Pair(currentPositionX, currentPositionY)
-    }
-}
-
-fun whenBackward(currentPositionZ: String, currentPositionX: Int, currentPositionY: Int): Pair<Int, Int> {
-    return when(currentPositionZ) {
-        "n" -> Pair(currentPositionX, currentPositionY - 1)
-        "s" -> Pair(currentPositionX, currentPositionY + 1)
-        "e" -> Pair(currentPositionX - 1, currentPositionY)
-        "w" -> Pair(currentPositionX + 1, currentPositionY)
-        else -> Pair(currentPositionX, currentPositionY)
+fun whenForward(points: Map<String, Any>): Pair<Int, Int> {
+    return when(points["z"]) {
+        "n" -> Pair(points["x"] as Int, points["y"] as Int + 1)
+        "s" -> Pair(points["x"] as Int, points["y"] as Int - 1)
+        "e" -> Pair(points["x"] as Int + 1, points["y"] as Int)
+        "w" -> Pair(points["x"] as Int - 1, points["y"] as Int)
+        else -> Pair(points["x"] as Int, points["y"] as Int)
     }
 }
 
-fun whenLeft(currentPositionZ: String): String {
-    return when(currentPositionZ) {
+fun whenBackward(points: Map<String, Any>): Pair<Int, Int> {
+    return when(points["z"]) {
+        "n" -> Pair(points["x"] as Int, points["y"] as Int - 1)
+        "s" -> Pair(points["x"] as Int, points["y"] as Int + 1)
+        "e" -> Pair(points["x"] as Int - 1, points["y"] as Int)
+        "w" -> Pair(points["x"] as Int + 1, points["y"] as Int)
+        else -> Pair(points["x"] as Int, points["y"] as Int)
+    }
+}
+
+fun whenLeft(points: Map<String, Any>): String {
+    return when(points["z"]) {
         "n" -> "w"
         "s" -> "e"
         "e" -> "n"
         "w" -> "s"
-        else -> currentPositionZ
+        else -> points["z"].toString()
     }
 }
 
-fun whenRight(currentPositionZ: String): String {
-    return when(currentPositionZ) {
+fun whenRight(points: Map<String, Any>): String {
+    return when(points["z"]) {
         "n" -> "e"
         "s" -> "w"
         "e" -> "s"
         "w" -> "n"
-        else -> currentPositionZ
+        else -> points["z"].toString()
     }
 }
 
--- test/MarsRoverTest.kt
+++ test/MarsRoverTest.kt
@@ -4,34 +4,34 @@ import kotlin.test.assertTrue
 class MarsRoverTest {
     @Test
     fun `given a position when moving forward then a new position is set`() {
-        val positionAndResult1 = Pair(Triple("n", 1, 1), Pair(1, 2))
-        val positionAndResult2 = Pair(Triple("s", 1, 1), Pair(1, 0))
-        val positionAndResult3 = Pair(Triple("e", 1, 1), Pair(2, 1))
-        val positionAndResult4 = Pair(Triple("w", 1, 1), Pair(0, 1))
+        val positionAndResult1 = Pair(mapOf("z" to "n", "x" to 5, "y" to 5), Pair(5, 6))
+        val positionAndResult2 = Pair(mapOf("z" to "s", "x" to 5, "y" to 5), Pair(5, 4))
+        val positionAndResult3 = Pair(mapOf("z" to "e", "x" to 5, "y" to 5), Pair(6, 5))
+        val positionAndResult4 = Pair(mapOf("z" to "w", "x" to 5, "y" to 5), Pair(4, 5))
 
         listOf(positionAndResult1, positionAndResult2, positionAndResult3, positionAndResult4).forEach {
-            assertTrue { whenForward(it.first.first, it.first.second, it.first.third) == it.second }
+            assertTrue { whenForward(it.first) == it.second }
         }
     }
 
     @Test
     fun `given a position when moving backward then a new position is set`() {
-        val positionAndResult1 = Pair(Triple("n", 1, 1), Pair(1, 0))
-        val positionAndResult2 = Pair(Triple("s", 1, 1), Pair(1, 2))
-        val positionAndResult3 = Pair(Triple("e", 1, 1), Pair(0, 1))
-        val positionAndResult4 = Pair(Triple("w", 1, 1), Pair(2, 1))
+        val positionAndResult1 = Pair(mapOf("z" to "n", "x" to 1, "y" to 1), Pair(1, 0))
+        val positionAndResult2 = Pair(mapOf("z" to "s", "x" to 1, "y" to 1), Pair(1, 2))
+        val positionAndResult3 = Pair(mapOf("z" to "e", "x" to 1, "y" to 1), Pair(0, 1))
+        val positionAndResult4 = Pair(mapOf("z" to "w", "x" to 1, "y" to 1), Pair(2, 1))
 
         listOf(positionAndResult1, positionAndResult2, positionAndResult3, positionAndResult4).forEach {
-            assertTrue { whenBackward(it.first.first, it.first.second, it.first.third) == it.second }
+            assertTrue { whenBackward(it.first) == it.second }
         }
     }
 
     @Test
     fun `given a position when moving left then a new position is set`() {
-        val positionAndResult1 = Pair("n", "w")
-        val positionAndResult2 = Pair("s", "e")
-        val positionAndResult3 = Pair("e", "n")
-        val positionAndResult4 = Pair("w", "s")
+        val positionAndResult1 = Pair(mapOf("z" to "n"), "w")
+        val positionAndResult2 = Pair(mapOf("z" to "s"), "e")
+        val positionAndResult3 = Pair(mapOf("z" to "e"), "n")
+        val positionAndResult4 = Pair(mapOf("z" to "w"), "s")
 
         listOf(positionAndResult1, positionAndResult2, positionAndResult3, positionAndResult4).forEach {
             assertTrue { whenLeft(it.first) == it.second }
@@ -40,10 +40,10 @@ class MarsRoverTest {
 
     @Test
     fun `given a position when moving right then a new position is set`() {
-        val positionAndResult1 = Pair("n", "e")
-        val positionAndResult2 = Pair("s", "w")
-        val positionAndResult3 = Pair("e", "s")
-        val positionAndResult4 = Pair("w", "n")
+        val positionAndResult1 = Pair(mapOf("z" to "n"), "e")
+        val positionAndResult2 = Pair(mapOf("z" to "s"), "w")
+        val positionAndResult3 = Pair(mapOf("z" to "e"), "s")
+        val positionAndResult4 = Pair(mapOf("z" to "w"), "n")
 
         listOf(positionAndResult1, positionAndResult2, positionAndResult3, positionAndResult4).forEach {
             assertTrue { whenRight(it.first) == it.second }
